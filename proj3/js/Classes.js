0f1db44d4d4781b2555b14d95cdedf9090364d78
Dpac99 <diogo.a.pacheco@tecnico.ulisboa.pt>
2019-11-04 16:42:56 +0000

    lighting done

--- proj3/js/Classes.js
+++ proj3/js/Classes.js
@@ -10,6 +10,187 @@ class Object3d extends THREE.Object3D {
   }
 }
 
+class Wall extends Object3d {
+  constructor(x, y, z, l, theta) {
+    super(x, y, z);
+    this.theta = theta;
+    this.l = l;
+
+    this.addWall();
+    this.addBottom();
+  }
+
+  addWall() {
+    let geometry = new THREE.CubeGeometry(this.l, 200, 2);
+    var material = new THREE.MeshLambertMaterial(
+        {color: 0xe8e4c9, side: THREE.DoubleSide});
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 100, 0);
+    mesh.rotation.y = this.theta;
+    mesh.receiveShadow = true;
+
+    this.add(mesh);
+  }
+
+  addBottom() {
+    let geometry = new THREE.CubeGeometry(this.l, 10, 5);
+    var material = new THREE.MeshLambertMaterial(
+        {color: 0x5d432c, side: THREE.DoubleSide});
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 5, 0);
+    mesh.rotation.y = this.theta;
+    mesh.receiveShadow = true;
+
+    this.add(mesh);
+  }
+}
+
+class Icosahedron extends Object3d {
+  constructor(x, y, z, r) {
+    super(x, y, z);
+    this.r = r;
+
+    this.material = new THREE.MeshLambertMaterial(
+        {color: 0xa9a9a9, side: THREE.DoubleSide});
+
+    var t = (1 + Math.sqrt(5)) / 2;
+
+    var vertices = [
+      -1,       t + 0.5, 0,  0.8,      t,       0, -1,      -t - 0.2, 0, 1,
+      -t + 0.2, 0,       0,  -1,       t + 0.5, 0, 1 - 0.3, t,        0, -1,
+      -t + 0.4, 0,       1,  -t + 0.5, t - 0.5, 0, -1,      t - 0.5,  0, 1,
+      -t + 0.7, 0,       -1, -t - 0.6, 0,       1
+    ];
+
+    /*var vertices = [
+      -1, t,  0,  1, t, 0,  -1, -t, 0,  1, -t, 0, 0,  -1, t,  0,  1, t,
+      0,  -1, -t, 0, 1, -t, t,  0,  -1, t, 0,  1, -t, 0,  -1, -t, 0, 1
+    ];*/ //Regular Icosahedron
+
+    var indices = [
+      0, 11, 5,  0, 5,  1, 0, 1, 7, 0, 7,  10, 0, 10, 11, 1, 5, 9, 5, 11,
+      4, 11, 10, 2, 10, 7, 6, 7, 1, 8, 3,  9,  4, 3,  4,  2, 3, 2, 6, 3,
+      6, 8,  3,  8, 9,  4, 9, 5, 2, 4, 11, 6,  2, 10, 8,  6, 7, 9, 8, 1
+    ];
+
+    this.geometry = new THREE.PolyhedronGeometry(vertices, indices, r);
+
+    var mesh = new THREE.Mesh(this.geometry, this.material);
+    mesh.position.set(0, r - 2 * t, 0);
+
+    mesh.castShadow = true;
+
+    this.add(mesh);
+  }
+}
+
+class Floor extends Object3d {
+  constructor(x, y, z, l, w) {
+    super(x, y, z);
+    this.l = l;
+    this.w = w;
+
+    this.addFloor();
+  }
+
+  addFloor() {
+    let geometry = new THREE.CubeGeometry(this.l, 1, this.w);
+    var material =
+        new THREE.MeshPhongMaterial({color: 0x86242a, side: THREE.DoubleSide});
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 0, 0);
+    mesh.receiveShadow = true;
+
+    this.add(mesh);
+  }
+}
+
+class Stand extends Object3d {
+  constructor(x, y, z, h) {
+    super(x, y, z);
+    this.h = h;
+    this.material = new THREE.MeshLambertMaterial(
+        {color: 0x464646, side: THREE.DoubleSide});
+
+    this.addSlab(0, 2.5, 0);
+    this.addArm(0, 5 + h / 2, 0);
+    this.addSlab(0, 7.5 + h, 0);
+    this.castShadow = true;
+  }
+
+  addSlab(x, y, z) {
+    let geometry = new THREE.CubeGeometry(this.h / 2, 5, this.h / 2);
+    var mesh = new THREE.Mesh(geometry, this.material);
+    mesh.position.set(x, y, z);
+    mesh.castShadow = true;
+
+    this.add(mesh);
+  }
+
+  addArm(x, y, z) {
+    let geometry = new THREE.CubeGeometry(5, this.h, 5);
+    var mesh = new THREE.Mesh(geometry, this.material);
+    mesh.position.set(x, y, z);
+    mesh.castShadow = true;
+
+    this.add(mesh);
+  }
+}
+
+class Spotlight extends Object3d {
+  constructor(x, y, z, obj) {
+    super(x, y, z);
+
+    this.coneMaterial =
+        new THREE.MeshPhongMaterial({color: 0x000000, side: THREE.DoubleSide});
+
+    this.bulbMaterial =
+        new THREE.MeshPhongMaterial({color: 0xffffff, side: THREE.DoubleSide});
+
+
+    this.addCone();
+    this.addSphere();
+    this.addLight(obj);
+
+    this.lookAt(obj.position);
+  }
+
+  addCone() {
+    var geo = new THREE.ConeGeometry(10, 20, 50, 5, true);
+    var mesh = new THREE.Mesh(geo, this.coneMaterial);
+    mesh.rotateX(-Math.PI / 2);
+    this.add(mesh);
+  }
+
+  addSphere() {
+    var geo = new THREE.SphereGeometry(2, 25, 25);
+    var mesh = new THREE.Mesh(geo, this.bulbMaterial);
+    this.add(mesh);
+  }
+
+  addLight(obj) {
+    var spotlight = new THREE.SpotLight(0xffffff, 0.8);
+    spotlight.position.set(0, 0, 0);
+    spotlight.castShadow = true;
+
+    spotlight.target = obj;
+
+    spotlight.shadow.mapSize.width = 1024;
+    spotlight.shadow.mapSize.height = 1024;
+
+    spotlight.shadow.camera.near = 0.5;
+    spotlight.shadow.camera.far = 1000;
+    spotlight.shadow.camera.fov = 30;
+    this.light = spotlight;
+
+    this.add(spotlight)
+  }
+
+  toggleLight() {
+    this.light.visible = !this.light.visible;
+  }
+}
+
 
 class Painting extends Object3d {
   constructor(x, y, z, k, l, r) {
@@ -25,7 +206,7 @@ class Painting extends Object3d {
     this.bgH = this.k * 3;
 
     this.bgMaterial =
-        new THREE.MeshBasicMaterial({color: 0x808080, side: THREE.DoubleSide});
+        new THREE.MeshPhongMaterial({color: 0x808080, side: THREE.DoubleSide});
     this.squareMaterial = new THREE.MeshLambertMaterial(
         {color: 0x000000, side: THREE.DoubleSide});
     this.circleMaterial =
@@ -33,6 +214,8 @@ class Painting extends Object3d {
 
     this.addBackground();
     this.addSquares();
+
+    this.castShadow = true;
   }
 
 
@@ -40,6 +223,7 @@ class Painting extends Object3d {
     let geometry = new THREE.CubeGeometry(this.bgL, this.bgH, this.bgW);
     var mesh = new THREE.Mesh(geometry, this.bgMaterial);
     mesh.position.set(0, this.bgH / 2, 0);
+    mesh.castShadow = true;
 
     this.add(mesh);
   }
--- proj3/js/proj.js
+++ proj3/js/proj.js
@@ -1,7 +1,13 @@
 /*global THREE*/
 
 var cameras = [], scene, renderer, camFactor = 10;
-var camera = 0;
+var camera = 1;
+
+var dirLight;
+var ico;
+var painting;
+
+var spotlights = [];
 
 function createCamera() {
   'use strict';
@@ -10,14 +16,16 @@ function createCamera() {
       window.innerWidth / -camFactor, window.innerWidth / camFactor,
       window.innerHeight / camFactor, window.innerHeight / -camFactor, 1, 1300);
 
+  cameraO.position.x = -100;
+  cameraO.position.y = 50;
   cameraO.position.z = 1000;
   cameras.push(cameraO);
 
   var cameraP = new THREE.PerspectiveCamera(
       50, window.innerWidth / window.innerHeight, 1, 1000);
-  cameraP.position.x = 100;
-  cameraP.position.y = 200;
-  cameraP.position.z = 200;
+  cameraP.position.x = -300;
+  cameraP.position.y = 400;
+  cameraP.position.z = 500;
   cameras.push(cameraP);
   cameraP.lookAt(scene.position);
 }
@@ -27,9 +35,60 @@ function createScene() {
 
   scene = new THREE.Scene();
 
-  var painting = new Painting(0, 0, 0, 20, 6, 2.13);
+  var backWall = new Wall(0, 0, -100, 500, 0);
+  scene.add(backWall);
+
+  var sideWall = new Wall(250, 0, 50, 300, Math.PI / 2);
+  scene.add(sideWall);
+
+  var floor = new Floor(0, 0, 50, 500, 300);
+  scene.add(floor);
+
+  painting = new Painting(-100, 50, -100, 20, 6, 2.13);
   scene.add(painting);
-  scene.add(new THREE.AxesHelper(5));
+
+  var stand = new Stand(80, 0, 0, 50);
+  scene.add(stand);
+
+  ico = new Icosahedron(80, 60, 0, 15);
+  scene.add(ico);
+
+  scene.add(new THREE.AxesHelper(10));
+}
+
+function createLights() {
+  dirLight = new THREE.DirectionalLight(0xffffff, 1);
+  dirLight.position.set(-250, 300, 200);
+
+  dirLight.shadow.camera.near = 2;
+  dirLight.shadow.camera.far = 1000;
+  dirLight.shadow.camera.left = -200;
+  dirLight.shadow.camera.right = 200;
+  dirLight.shadow.camera.top = 200;
+  dirLight.shadow.camera.bottom = -200;
+
+  dirLight.castShadow = true;
+
+  scene.add(dirLight);
+
+  helper = new THREE.DirectionalLightHelper(dirLight, 5);
+  scene.add(helper);
+
+  var s1 = new Spotlight(40, 150, 100, ico);
+  scene.add(s1);
+  spotlights.push(s1);
+
+  var s2 = new Spotlight(120, 150, 100, ico);
+  scene.add(s2);
+  spotlights.push(s2);
+
+  var s3 = new Spotlight(-160, 10, -30, painting);
+  scene.add(s3);
+  spotlights.push(s3);
+
+  var s4 = new Spotlight(-40, 10, -30, painting);
+  scene.add(s4);
+  spotlights.push(s4);
 }
 
 function render() {
@@ -63,10 +122,25 @@ function onResize() {
 function onKeyDown(e) {
   switch (e.keyCode) {
     case 49:  // 1
-      camera = 0;
+      spotlights[0].toggleLight()
       break;
     case 50:  // 2
+      spotlights[1].toggleLight()
+      break;
+    case 51:  // 3
+      spotlights[2].toggleLight()
+      break;
+    case 52:  // 4
+      spotlights[3].toggleLight()
+      break;
+    case 53:  // 5
+      camera = 0;
+      break;
+    case 54:  // 6
       camera = 1;
+      break;
+    case 81:  // Q
+      dirLight.visible = !dirLight.visible;
       break;
   }
 }
@@ -89,10 +163,13 @@ function init() {
   renderer = new THREE.WebGLRenderer();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMapSoft = true;
 
   document.body.appendChild(renderer.domElement);
 
   createScene();
+  createLights();
   createCamera();
 
   window.addEventListener('resize', onResize);
